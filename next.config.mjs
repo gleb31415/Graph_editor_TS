bfa3fae2a38cbc73a69b32ce46dc8b49d179448b
oxyanma <oxyanma@gmail.com>
2025-07-05 00:07:35 +0300

    edit of the next error indicator

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  devIndicators: {
+    buildActivity: false,
+    buildActivityPosition: 'bottom-right'
+  },
+  onDemandEntries: {
+    maxInactiveAge: 25 * 1000,
+    pagesBufferLength: 2,
+  },
+  experimental: {
+    // This will disable the error overlay
+    errorOverlay: false
+  }
+};
 
 export default nextConfig;
