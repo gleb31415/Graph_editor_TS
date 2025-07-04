2f0320e11641e8dcdf4fbbe69aec7bceb0a57516
oxyanma <oxyanma@gmail.com>
2025-07-04 22:50:12 +0300

    added styled-components

--- src/components/CustomNode/CustomNode.js
+++ src/components/CustomNode/CustomNode.js
@@ -1,32 +1,49 @@
 import React from "react";
 import { Handle, Position } from "reactflow";
-import styles from "./CustomNode.module.css";
+import styled from "styled-components";
+
+const CustomNodeWrap = styled.div`
+    padding: 10px;
+    border: 2px solid #0066ff;
+    border-radius: 8px;
+    background: #e6f0ff;
+    display: flex;
+    align-items: center;
+    font-size: 12px;
+`;
+
+const CustomHandle = styled(Handle)`
+    width: 8px;
+    height: 8px;
+    background: #0066ff;
+    border-radius: 50%;
+`;
+
+const NodeContent = styled.div`
+  flex: 1;
+  text-align: left;
+  padding: 0 10px;
+`;
+
+const NodeTitle = styled.div`
+  font-weight: bold;
+  margin-bottom: 4px;
+`;
 
 export default function CustomNode({ data }) {
   return (
-    <div className={styles.customNode}>
-      <Handle
+    <CustomNodeWrap>
+      <CustomHandle
         type="target"
         position={Position.Left}
-        className={styles.handle}
       />
-      <div className={styles.content}>
-        <div className={styles.title}>{data.title}</div>
-        {/* <div className={styles.section}>Раздел: {data.section}</div>
-        <div className={styles.grade}>Класс: {data.grade}</div> */}
-        {/* <div className={styles.status}>
-          <span
-            className={styles.statusCircle}
-            style={{ backgroundColor: data.completed ? "#0066ff" : "#ccc" }}
-          />
-        {data.completed ? "Выполнено" : "Не выполнено"}
-        </div> */}
-      </div>
-      <Handle
+      <NodeContent>
+        <NodeTitle>{data.title}</NodeTitle>
+      </NodeContent>
+      <CustomHandle
         type="source"
         position={Position.Right}
-        className={styles.handle}
       />
-    </div>
+    </CustomNodeWrap>
   );
 }
